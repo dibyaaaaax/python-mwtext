7023c4bf03c1ef8615fc20f6b1f429542dd778f0
Dibyaaaaax <Dibyaaaaax@local>
2020-07-13 05:15:15 +0000

    use sitelink titles to filter

--- mwtext/filter_functions/wikidata_items_with_wikipedia_sitelinks.py
+++ mwtext/filter_functions/wikidata_items_with_wikipedia_sitelinks.py
@@ -1,6 +1,50 @@
 import json
+import requests
+from collections import defaultdict
 
 import mwbase
+import mwapi
+
+from ..utilities.util import get_siteinfo
+
+def get_wiki_languages():
+    session = mwapi.Session("https://commons.wikimedia.org", user_agent = "")
+    doc = session.get(action="sitematrix", smtype="language")
+    sitematrix = doc['sitematrix']
+
+    lang_wiki = defaultdict(str)
+    for lang in sitematrix.keys():
+        if not lang.isdigit():
+            continue
+
+        lang_code = sitematrix[lang].get('code')
+        site = sitematrix[lang].get('site')
+        wiki_host = ''
+        for s in site:
+            if s.get('code', None) == "wiki" and s.get('closed', None) is None:
+                wiki_host = s.get('url')
+        if wiki_host:
+            lang_wiki[lang_code + "wiki"] = wiki_host
+
+    return lang_wiki
+
+
+def get_namespace_names():
+    languages = get_wiki_languages()
+    namespace_names = defaultdict(list)
+    for lang in languages:
+        session = mwapi.Session(languages[lang], user_agent = "")
+        doc = get_siteinfo(session)
+        namespaces = doc['namespaces']
+        for n in namespaces:
+            if namespaces[n].get('id') == 0:
+                continue
+            namespace_name = namespaces[n].get('name')
+            namespace_names[lang].append(namespace_name)
+
+    return namespace_names
+
+namespace_names = get_namespace_names()
 
 
 def include(page, revision):
@@ -16,7 +60,15 @@ def include(page, revision):
     if qid is None:
         return False
 
-    # Sitelinks to a Wikipedia of any language
-    return any((llink not in ('commonswiki', 'specieswiki') and
-                llink.endswith("wiki"))
-               for llink in entity.sitelinks)
+    # Sitelinks to a Wikipedia article
+    for llink in entity.sitelinks:
+        if llink not in namespace_names:
+            continue
+
+        title = entity.sitelinks[llink].get('title')
+        is_article = not any(title.startswith(restricted_namespace + ":")
+                         for restricted_namespace in namespace_names[llink])
+        if is_article:
+            return True
+
+    return False
